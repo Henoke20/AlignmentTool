7e03456ac8f12f48c64cd54e97c8adcfd13ca951
Henoke <hkshiferaw@gmail.com>
2016-05-28 23:07:26 -0400

    Added warning dialog when user enters wrong parameters

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import Alignment.GlobalAlign;
 import Alignment.LocalAlign;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
@@ -16,18 +17,30 @@ public class Controller {
 
     public void handleSaySup(ActionEvent actionEvent) {
 
-        // calls local aligner or Global aligner depending on which Raido Button Selected
-        if (myToggleGroup.getSelectedToggle().equals(Local)) {
 
-            LocalAlign lAligner = LocalAlign.GetLocalAlignInstance();
-            lAligner.FillTable(Integer.parseInt(Gap.getText()), Integer.parseInt(Match.getText())
-                    , Integer.parseInt(Mismatch.getText()), SequenceA.getText(), SequenceB.getText());
-        } else if (myToggleGroup.getSelectedToggle().equals(Global)) {
 
-            GlobalAlign gAligner = GlobalAlign.GetGlobalAlignInstance();
-            gAligner.FillTable(Integer.parseInt(Gap.getText()), Integer.parseInt(Match.getText())
-                    , Integer.parseInt(Mismatch.getText()), SequenceA.getText(), SequenceB.getText());
+        try {
+            // calls local aligner or Global aligner depending on which Raido Button Selected
+            if (myToggleGroup.getSelectedToggle().equals(Local)) {
+
+                LocalAlign lAligner = LocalAlign.GetLocalAlignInstance();
+                lAligner.FillTable(Integer.parseInt(Gap.getText()), Integer.parseInt(Match.getText())
+                        , Integer.parseInt(Mismatch.getText()), SequenceA.getText(), SequenceB.getText());
+            } else if (myToggleGroup.getSelectedToggle().equals(Global)) {
+
+                GlobalAlign gAligner = GlobalAlign.GetGlobalAlignInstance();
+                gAligner.FillTable(Integer.parseInt(Gap.getText()), Integer.parseInt(Match.getText())
+                        , Integer.parseInt(Mismatch.getText()), SequenceA.getText(), SequenceB.getText());
+            }
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning Dialog");
+            alert.setHeaderText("Warning");
+            alert.setContentText("Double check your parameters !!!");
+
+            alert.showAndWait();
         }
+
 
     }
 }
